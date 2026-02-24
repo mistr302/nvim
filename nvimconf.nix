7e4577ac7518b388f75fe55a325cd9067d5c4c88
mistr302 <michal.stransky42@gmail.com>
2026-02-24 10:50:10 +0100

    gd definition

--- nvimconf.nix
+++ nvimconf.nix
@@ -130,6 +130,8 @@ lib: {
       vim.keymap.set("x", "<S-Tab>", "<gv")
       vim.keymap.set("i", "<S-Tab>", "<C-D>")
 
+        vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
+        vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
     '';
 
     git = {
